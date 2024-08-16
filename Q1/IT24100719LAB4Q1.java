98486b6e62fed5bcd9e310379fb8eaf366266072
IT24100719 <it24100719@my.sliit.lk>
2024-08-16 09:14:29 +0530

    uploading assignment

---
+++ Q1/IT24100719LAB4Q1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+
+public class IT24100719LAB4Q1 {
+  public static void main(String[] args) {
+    Scanner pinput = new Scanner(System.in);
+
+    
+
+   
+      System.out.print("Enter a Number:");
+      int  Number1 = pinput.nextInt();
+     
+      if (Number1 == 0){
+        System.out.print("The Number is Zero.");
+      }
+      else if(Number1 > 0){
+        System.out.print("The Number is Positive.");
+      }
+      else{
+        System.out.print("The Number is Negative.");
+      }
+   
+  }
+
+
+    
+       
+
+  }
+
